2cf8156dae1a067132434fff561485e668bbca09
lamhotjm <lamhot.siagian@modalku.co.id>
2016-10-12 18:16:15 +0700

    update middlewares.py

--- yellowpage/middlewares.py
+++ yellowpage/middlewares.py
@@ -1,24 +1,10 @@
-__author__ = 'MisT'
-from scrapy.downloadermiddlewares.redirect import RedirectMiddleware
-from scrapy import log
-import os
-from urlparse import urljoin
+# -*- coding: utf-8 -*-
 
+import random
+from settings import PROXIES
 
-class ypRedirect(RedirectMiddleware):
-    def process_response(self, request, response, spider):
-        print 'ypRedirect...'
-        log.msg("ypRedirect...", level=log.INFO)
-        if request.method == 'HEAD':
-            if response.status in [301, 302, 303, 307] and 'Location' in response.headers:
-                redirected_url = urljoin(request.url, response.headers['location'])
-                print '302(h):', redirected_url
-                os.system("pause")
-            else:
-                return response
+class ProxyMiddleware(object):
+    def process_request(self, request, spider):
+        proxy = random.choice(PROXIES)
+        request.meta['proxy'] = "http://%s" % proxy['ip_port']
 
-        if response.status in [302, 303] and 'Location' in response.headers:
-            redirected_url = urljoin(request.url, response.headers['location'])
-            print '302:', redirected_url
-            os.system("pause")
-        return response
